1ba70e2d09f0a884755e7333c5eb465d5599b9a0
srgg6701 <srgg.next@gmail.com>
2025-09-04 21:35:22 +0300

    Setting rewrites in next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://138.124.31.142:3001/api/:path*',
+      },
+    ];
+  },};
 
 module.exports = nextConfig;
